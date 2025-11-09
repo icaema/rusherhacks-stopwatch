77a9d10c777ce02d1752671f9305c3e3640f6b5a
icaema <icaema@protonmail.com>
2025-11-08 20:53:20 -0500

    Formalized Laps Prerelease 2

--- src/main/java/org/icaema/StopwatchTime.java
+++ src/main/java/org/icaema/StopwatchTime.java
@@ -24,8 +24,32 @@ public class StopwatchTime {
         long seconds = duration.toSecondsPart();
         long millis = duration.toMillisPart();
 
-        return String.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, millis);
+        return String.format("%02d:%02d:%02d.%02d", hours, minutes, seconds, millis/10);
     }
+
+    private String convertTicksToDiffString(int t) {
+        long milliseconds = t * 50L;
+        Duration duration = Duration.ofMillis(milliseconds);
+        long hours = duration.toHours();
+        long minutes = duration.toMinutesPart();
+        long seconds = duration.toSecondsPart();
+        long millis = duration.toMillisPart();
+
+        StringBuilder tempDiffStringBuilder = new StringBuilder();
+
+        if (hours > 0) tempDiffStringBuilder.append(hours).append("h");
+        if (minutes > 0) tempDiffStringBuilder.append(minutes).append("m");
+        if (seconds > 0 || millis > 0 || tempDiffStringBuilder.isEmpty()) {
+            if (millis > 0) {
+                tempDiffStringBuilder.append(String.format("%d.%02ds", seconds, millis/10));
+            } else {
+                tempDiffStringBuilder.append(seconds).append("s");
+            }
+        }
+
+        return tempDiffStringBuilder.toString();
+    }
+
 
     static {
         instance = new StopwatchTime();
@@ -49,14 +73,31 @@ public class StopwatchTime {
     }
 
     public void lap() {
-        laps.add(ticks);
+        laps.addFirst(ticks);
     }
 
     public String toTimeString() {
         return convertTicksToTimeString(ticks);
     }
 
+    public int lapsLength() {
+        return laps.size();
+    }
+
     public List<String> getLapStrings() {
-        return laps.stream().map(this::convertTicksToTimeString).toList();
+        if (laps.isEmpty()) return new ArrayList<>();
+
+        List<String> lapStrings = new ArrayList<>();
+
+        for  (int i = 0; i < laps.size(); i++) {
+            String tempLapString = convertTicksToTimeString(laps.get(i));
+            if (i==laps.size()-1) {
+                lapStrings.add(tempLapString);
+                continue;
+            }
+            tempLapString += "\t+" + convertTicksToDiffString(laps.get(i)-laps.get(i+1));
+            lapStrings.add(tempLapString);
+        }
+        return lapStrings;
     }
 }
